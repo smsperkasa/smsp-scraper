85f75cf825a5031d2f25827262cbd528a6581645
brianadhitya <brian@smsperkasa.com>
2024-07-23 15:27:57 +0700

    Major scraping changes

--- script.py
+++ script.py
@@ -1,4 +1,17 @@
+import pandas as pd
 import yfinance as yf
 
-historical_data = yf.download("USDIDR=X", start="2020-01-01", end="2020-12-31")
-print(historical_data)
+from index import SheetUploader
+
+cnyusd_data = yf.download("CNYUSD=X", start="2022-12-13", end="2022-12-31")
+usdidr_data = yf.download("USDIDR=X", start="2022-12-13", end="2022-12-31")
+cnyusd_data = cnyusd_data["Close"]
+usdidr_data = usdidr_data["Close"]
+historical_data = pd.DataFrame(cnyusd_data * usdidr_data, columns=["Close"])
+sheet_uploader = SheetUploader()
+
+for i, r in historical_data.iterrows():
+    date_str = i.strftime("%Y-%m-%d")
+    sheet_uploader.upload_data_raw(
+        "Yuan", [date_str, "SMSP Scraper", "Yahoo Finance", r["Close"], "-"]
+    )
